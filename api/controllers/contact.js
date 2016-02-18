1dcd9c7c68501132973fc89d74dd62ba39d78c3f
Mohsen Azimi <me@azimi.me>
2016-02-18 09:53:54 -0800

    More implementations

--- api/controllers/contact.js
+++ api/controllers/contact.js
@@ -1,25 +1,51 @@
 const db = require('./db');
+const _ = require('lodash');
 
 module.exports = {
 
   // GET /contact/{id}
   get(req, res) {
-    const id = req.swagger.params.id;
-    const contact = db.filter(contact=> contact.id === id)[0];
+    const id = req.swagger.params.id.value;
+    const contact = _.find(db, contact=> contact.id === id);
 
     if (!contact) {
-      return res.status(404).send('Not found');
+      return res.sendStatus(404);
     }
     res.json(contact);
   },
 
   // PUT /contact/{id}
   put(req, res) {
-    res.json({task: 'TODO'});
+    const id = req.swagger.params.id.value;
+    const body = req.swagger.params.body.value;
+    const isSameContact = contact=> contact.id === id;
+
+    const contact = _.find(db, isSameContact);
+
+    if (!contact) {
+      return res.sendStatus(404);
+    }
+
+    _.remove(db, isSameContact)
+
+    db.push(body);
+
+    res.json(body);
   },
 
   // DELETE /contact/{id}
   delete(req, res) {
-    res.json({task: 'TODO'});
+    const id = req.swagger.params.id.value;
+    const isSameContact = contact=> contact.id === id;
+
+    const contact = _.find(db, isSameContact);
+
+    if (!contact) {
+      return res.sendStatus(404);
+    }
+
+    _.remove(db, isSameContact)
+
+    res.json(body);
   }
 }
