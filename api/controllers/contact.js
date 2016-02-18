0c7a9d15b5f20a7beea63b845e45945655a32b29
Mohsen Azimi <me@azimi.me>
2016-02-17 23:02:27 -0800

    Project skeleton

---
+++ api/controllers/contact.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  // GET /contact/{id}
+  get(req, res) {
+    res.json({task: 'TODO'});
+  },
+
+  // PUT /contact/{id}
+  put(req, res) {
+    res.json({task: 'TODO'});
+  },
+
+  // DELETE /contact/{id}
+  delete(req, res) {
+    res.json({task: 'TODO'});
+  }
+}
---
+++ api/controllers/contacts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+
+  // GET /contacts
+  get(req, res) {
+    res.json({task: 'TODO'});
+  },
+
+  // POST /contact/{id}
+  post(req, res) {
+    res.json({task: 'TODO'});
+  }
+}
